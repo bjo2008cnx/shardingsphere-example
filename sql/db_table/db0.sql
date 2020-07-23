a5664476042bf0f1dd77c20c8f759e0b9802fac1
pili <wangxm@>
2020-07-23 20:37:13 +0800

    master slave is ok

--- sql/db_table/db0.sql
+++ sql/db_table/db0.sql
@@ -1,45 +1,8 @@
 # 既分表，又分库
 
-CREATE SCHEMA `shardingsphere-demo0-master` DEFAULT CHARACTER SET utf8 ;
+CREATE SCHEMA `demo0` DEFAULT CHARACTER SET utf8 ;
 
-use `shardingsphere-demo0-master`;
-
-CREATE TABLE `user_0` (
-  `id` BIGINT NOT NULL ,
-  `name` varchar(45) DEFAULT NULL,
-  `city` varchar(45) DEFAULT NULL,
-  `data_source` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE `user_1` (
-  `id` BIGINT NOT NULL ,
-  `name` varchar(45) DEFAULT NULL,
-  `city` varchar(45) DEFAULT NULL,
-  `data_source` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE `user_2` (
-  `id` BIGINT NOT NULL ,
-  `name` varchar(45) DEFAULT NULL,
-  `city` varchar(45) DEFAULT NULL,
-  `data_source` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE `user_3` (
-  `id` BIGINT NOT NULL ,
-  `name` varchar(45) DEFAULT NULL,
-  `city` varchar(45) DEFAULT NULL,
-  `data_source` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4;
-
-# slave
-CREATE SCHEMA `shardingsphere-demo0-slave` DEFAULT CHARACTER SET utf8 ;
-
-use `shardingsphere-demo0-slave`;
+use `demo0`;
 
 CREATE TABLE `user_0` (
   `id` BIGINT NOT NULL ,
@@ -72,5 +35,4 @@ CREATE TABLE `user_3` (
   `data_source` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4;
-
 
